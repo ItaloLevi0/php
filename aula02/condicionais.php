973b4449485f8f29690b6088f5db88761250f354
ItaloLevi0 <jorgelennon.meneses@gmail.com>
2022-11-11 11:07:16 -0300

    resolucao da questao 2

--- aula02/condicionais.php
+++ aula02/condicionais.php
@@ -1,22 +1,33 @@
 <?php
 
-$altura = $_GET["altura"];
-$sexo = strtolower($_GET["sexo"]);
 
-if($sexo != "m" && $sexo != "f") {
-    echo "Sexo não identificado";
-} else {
-    if ($sexo == "m") {
-        $imc = (72.7 * $altura) -58;
-        echo "Imc: ".number_format($imc, 1);
-    }
+// $nome = "sandra";
+// $sexo = "f";
+// $estadoCivil = "casada";
 
-    if ($sexo == "f") {
-        $imc = (62.1 * $altura) -44.7;
-        echo "Imc: ".number_format($imc, 1);
-    }
+// if($sexo == "f" && $estadoCivil == "casada") {
+//     echo "<script>let tempo = prompt('Quanto anos de casada?'); document.write(tempo >2 ? 'Ok': 'Esperar mais')</script>";
+// }
 
-}
+
+
+// $altura = $_GET["altura"];
+// $sexo = strtolower($_GET["sexo"]);
+
+// if($sexo != "m" && $sexo != "f") {
+//     echo "Sexo não identificado";
+// } else {
+//     if ($sexo == "m") {
+//         $imc = (72.7 * $altura) -58;
+//         echo "Imc: ".number_format($imc, 1);
+//     }
+
+//     if ($sexo == "f") {
+//         $imc = (62.1 * $altura) -44.7;
+//         echo "Imc: ".number_format($imc, 1);
+//     }
+
+// }
 
 
 // $peso = $_GET["peso"];
